4d169b0e1df6e10a472d511c5b17c354f8dcff05
elhectorlitro <87398353+elhectorlitro@users.noreply.github.com>
2021-07-27 13:09:44 -0500

    Create Binary_Search.py

---
+++ Binary_Search.py
@@ -0,0 +1,18 @@
+from random import randint
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
+value = randint(0,20) 
+tries = 0;    
+def Binary_Search(value, numbers, tries): 
+  if tries==0:
+    low = 0                                                                                                     
+    high = len(numbers)                                                                                          
+    mid = (high-low/2)                                                                                           
+    else:                                                                                                            
+      if mid==value:                                                                                                                                                                                                                 
+        elif mid < value:
+          else :                 
+          print("Is the number"+str(mid)+"?")
+          print("Number of iteration: "+str(tries)) 
+    return;                                                                                                        
+  Binary_Search(value, numbers, tries) 
