05710a3211ed3b88d5b15b0d09a9a05880a02c27
elhectorlitro <hectormanuelcr98@gmail.com>
2021-07-30 18:08:15 -0500

    Generate list function working

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,7 +1,17 @@
-from random import randint
+import random
 
-def SelectionSort() 
-  
+def GenerateRandomList(Mini, Maxi, Num_of_elements):
+  ran_list = []
+  for i in range (0,Num_of_elements):
+      ran_value = random.randint(Mini,Maxi)
+      ran_list.append(ran_value)
+  return ran_list
+
+def SelectionSort(UnSortList):
+  Copy = []
+
   return 0
+
+print(GenerateRandomList(2,9,3))
 
 
