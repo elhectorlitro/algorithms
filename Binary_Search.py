a01016ba80a64d4e0d4c70cce68c03993b7b3c30
elhectorlitro <hectormanuelcr98@gmail.com>
2021-07-30 12:04:11 -0500

    Can be improved but already works fine

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,27 +1,23 @@
 from random import randint
 
-def Binary_Search(value, numbers, tries, maxi, mini, midi):
+def Binary_Search(value, numbers, tries, maxi, mini):
     tries+=1
-    print("Is it")
-    print(midi)
-    print("?")
     mid=round(((maxi-mini)/2)+mini)
-    if midi == value :
+    print("Is it "+str(mid)+"?")
+    if mid == value :
         print("Yes")
-        return [mid, tries]
-    elif midi > value:
+        return mid, tries
+    elif mid > value:
         print("No")
-        print(mid)
-        Binary_Search(value, numbers, tries, midi, mini,mid)
+        return Binary_Search(value, numbers, tries, mid, mini)
     else : 
         print("No")
-        print(mid)
-        Binary_Search(value, numbers, tries, maxi, midi,mid)
-    return;
+        return Binary_Search(value, numbers, tries, maxi, mid)
+    return 0 
+
 
 numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]#sorted list of int numbers 
 to_guess = randint(0,20)
-print(to_guess)
-to_print = Binary_Search(to_guess, numbers, 0, len(numbers),0,(len(numbers)-0)/2)
-print("[Number guessed, number of tries]:")
-print(to_print)
+print("Number to be guessed: "+str(to_guess))
+number_guessed, number_of_tries = Binary_Search(to_guess, numbers, 0, len(numbers),0)
+print("Number "+ str(number_guessed)+" after "+str(number_of_tries)+" tries")
