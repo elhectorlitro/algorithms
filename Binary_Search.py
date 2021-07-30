d9a8503b6d117af3409fb7d658f38acbefcd73e2
elhectorlitro <hectormanuelcr98@gmail.com>
2021-07-29 19:55:14 -0500

    already works but it has bug

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,18 +1,27 @@
 from random import randint
 
+def Binary_Search(value, numbers, tries, maxi, mini, midi):
+    tries+=1
+    print("Is it")
+    print(midi)
+    print("?")
+    mid=round(((maxi-mini)/2)+mini)
+    if midi == value :
+        print("Yes")
+        return [mid, tries]
+    elif midi > value:
+        print("No")
+        print(mid)
+        Binary_Search(value, numbers, tries, midi, mini,mid)
+    else : 
+        print("No")
+        print(mid)
+        Binary_Search(value, numbers, tries, maxi, midi,mid)
+    return;
+
 numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]#sorted list of int numbers 
-value = randint(0,20) 
-tries = 0;    
-def Binary_Search(value, numbers, tries): 
-  if tries==0:
-    low = 0                                                                                                     
-    high = len(numbers)                                                                                          
-    mid = (high-low/2)                                                                                           
-    else:                                                                                                            
-      if mid==value:                                                                                                                                                                                                                 
-        elif mid < value:
-          else :                 
-          print("Is the number"+str(mid)+"?")
-          print("Number of iteration: "+str(tries)) 
-    return;                                                                                                        
-  Binary_Search(value, numbers, tries) 
+to_guess = randint(0,20)
+print(to_guess)
+to_print = Binary_Search(to_guess, numbers, 0, len(numbers),0,(len(numbers)-0)/2)
+print("[Number guessed, number of tries]:")
+print(to_print)
