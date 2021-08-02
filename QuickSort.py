c363683ac8c048919f1d79cd02a6312e77e501fb
elhectorlitro <hectormanuelcr98@gmail.com>
2021-08-02 13:41:23 -0500

    Works accordingly but needs more tests

--- QuickSort.py
+++ QuickSort.py
@@ -12,18 +12,17 @@ def QuickSort(unsorted_list):
     else :
         pivot = int(len(unsorted_list)/2)
         compare = unsorted_list[pivot]
-        print("pivot: "+str(compare))
         less = []
         more = []
         pivot_list = []
-        for i in range(0,len(unsorted_list)-1):
-            if compare < unsorted_list[i]: more.append(unsorted_list[i])
-            else : less.append(unsorted_list[i])
-        pivot_list.append(pivot)
-        print("Lower elements: " + str(less))
-        print("Pivot: " + str(pivot_list))
-        print("Upper elements: " + str(more))
-        return QuickSort(less) + pivot_list + QuickSort(more)
+        for i in range(0,len(unsorted_list)):
+            if compare < unsorted_list[i] and i != pivot:
+                more.append(unsorted_list[i])
+            elif compare >= unsorted_list[i] and i != pivot:
+                less.append(unsorted_list[i])
+            else : 
+                pivot_list.append(compare)
+        return QuickSort(less) + QuickSort(pivot_list) + QuickSort(more)
 
 to_sort = GenerateRandomList(2,14,8)
 print(to_sort)
