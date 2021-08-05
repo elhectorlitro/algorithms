50829e61fab45f70cf59f7afae59b59f542352fd
elhectorlitro <hectormanuelcr98@gmail.com>
2021-08-05 14:53:32 -0500

    Seems to already be able to find any node

--- Breath-firstSearch.py
+++ Breath-firstSearch.py
@@ -12,7 +12,7 @@ graph["f"] = ["origin"]
 graph["g"] = ["h", "p"]
 graph["h"] = ["a", "r"]
 graph["g"] = ["h", "p"]         
-graph["h"] = ["a", "r"]
+graph["h"] = ["a", "r", "g"]
 graph["i"] = ["b"]
 graph["j"] = ["b"]
 graph["k"] = ["s", "t", "l"]
@@ -34,21 +34,43 @@ graph["z"] = ["y"]
 
 def BreadthFirstSearch(gra, dep, obj):#You give the graph, element you're departuring from and the node that is your objective
    queue = []#Creates a new queue
-   queue.append(gra[dep])#You add the departing node as the first on the queue
+   visited = []
+   queue.append(dep)#You add the departing node as the first on the queue
    while queue: #Until the queue gets empty 
-     node = queue.pop() #pop the queue
+     key = queue.pop() #pop the queue
+     print("current key: " + key)
+     node = gra[key]
+     print("Tried with: "+str(node))
      if Contains(node, obj) : 
           print("Found a way to the node")
           return True
-     else : 
-          queue.append(gra["node"])#If it does not contain, a path to the objective, add the node to the queue
+     else :
+          visited.append(key)
+          for each_key in node:
+             print("Checking: " + each_key)
+             if (Check(visited,each_key)) :
+                  print(str(each_key)+" already visited")
+             else :
+                  print(str(each_key)+" not visited yet")
+                  queue.append(each_key)#If it does not contain, a path to the objective, add the node to the queue
+                  print(str(each_key)+" appended")
    return False
 
 def Contains(node, obj):
      if obj in node:
-         print(str(obj)+" contained") 
-         return True
-     else : return False
+       print(str(obj)+" contained") 
+       return True
+     else : 
+       return False
 
-print(BreadthFirstSearch(graph,"origin","a"))
+def Check(Visited,key):
+    if key in Visited:
+       return True
+    else :
+       return False
+print("Type the node you want to departure from: ")
+departure = str(input())
+print("Type the node you want to reach to: ")
+objective = str(input())
+print(BreadthFirstSearch(graph,departure,objective))
 
