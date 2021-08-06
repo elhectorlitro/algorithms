f61457e3af81ea9512650ef6b018e95ab786ec33
elhectorlitro <hectormanuelcr98@gmail.com>
2021-08-06 13:38:12 -0500

    Looks to be able to find the shortest path

--- Breath-firstSearch.py
+++ Breath-firstSearch.py
@@ -15,7 +15,7 @@ graph["g"] = ["h", "p"]
 graph["h"] = ["a", "r", "g"]
 graph["i"] = ["b"]
 graph["j"] = ["b"]
-graph["k"] = ["s", "t", "l"]
+graph["k"] = ["b","s", "t", "l"]
 graph["l"] = ["k"]
 graph["m"] = ["c", "u", "v"]
 graph["n"] = ["d", "w"]
@@ -35,39 +35,41 @@ graph["z"] = ["y"]
 def BreadthFirstSearch(gra, dep, obj):#You give the graph, element you're departuring from and the node that is your objective
    queue = []#Creates a new queue
    visited = []
+   way = []
    queue.append(dep)#You add the departing node as the first on the queue
    while queue: #Until the queue gets empty 
-     key = queue.pop() #pop the queue
-     print("current key: " + key)
+     key = queue.pop() #pop the queue    
      node = gra[key]
-     print("Tried with: "+str(node))
-     if Contains(node, obj) : 
+     way.append(key)    
+     if Check(node, obj) : 
+          way.append(obj)
           print("Found a way to the node")
+          print("way: ")
+          print(way)
+          print("Visited: ")
+          print(visited)
           return True
      else :
           visited.append(key)
+          way = CheckPath(visited, way, key, gra)
           for each_key in node:
-             print("Checking: " + each_key)
-             if (Check(visited,each_key)) :
-                  print(str(each_key)+" already visited")
-             else :
-                  print(str(each_key)+" not visited yet")
-                  queue.append(each_key)#If it does not contain, a path to the objective, add the node to the queue
-                  print(str(each_key)+" appended")
+             if (Check(visited,each_key))==False :#If it is not on the visited record append it
+                queue.append(each_key)#If it does not contain, a path to the objective, add the node to the queue
    return False
 
-def Contains(node, obj):
-     if obj in node:
-       print(str(obj)+" contained") 
-       return True
-     else : 
-       return False
-
-def Check(Visited,key):
-    if key in Visited:
+def Check(node,key):
+    if key in node:
        return True
     else :
        return False
+
+def CheckPath(Visited, Way, Key, Gra) : 
+    check = all(item in Visited for item in Gra[Key])
+    if check is False :
+       return Way
+    else : 
+       return CheckPath(Visited, Way[:Way.index(Key)],Way[Way.index(Key)-1],Gra)
+
 print("Type the node you want to departure from: ")
 departure = str(input())
 print("Type the node you want to reach to: ")
